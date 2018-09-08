34d0b3af786fd2a471c3c6050084e2d860ca69e2
Arturo Collazo <accg14@gmail.com>
2018-09-08 15:25:47 -0300

    +Factory bots +rspec (some issues not resolved)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,21 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 <<<<<<< HEAD
+<<<<<<< HEAD
   describe 'validations' do
     let!(:organization) { FactoryBot.create(:organization) }
     let!(:user) { FactoryBot.build(:user, organization: organization) }
 
     it 'should let create new ones, with appropiate data' do
       expect(user).to be_valid
+=======
+  describe 'validations' do
+    let!(:organization) { FactoryBot.build(:organization) }
+    let!(:user) { FactoryBot.build(:user, organization: organization) }
+
+    it 'should let create new ones, with appropiate data' do
+      expect(user). to be_valid
+>>>>>>> +Factory bots +rspec (some issues not resolved)
     end
 
     it 'should not let create new ones, with nil ci value' do
@@ -15,6 +24,7 @@ RSpec.describe User, type: :model do
       expect(user).not_to be_valid
     end
 
+<<<<<<< HEAD
     it 'should not let create new ones, with nil name value' do
       user.name = nil
       expect(user).not_to be_valid
@@ -28,4 +38,14 @@ RSpec.describe User, type: :model do
 =======
   pending "add some examples to (or delete) #{__FILE__}"
 >>>>>>> Organization & User: +Model,+Controller,+Serializer,+Under active admin. Not done yet
+=======
+    #it 'should not let create new ones, with ci value already taken' do
+#
+ #   end
+
+  #  it 'should not let create new ones, with email value already taken' do
+#
+ #   end
+  end
+>>>>>>> +Factory bots +rspec (some issues not resolved)
 end
