d0493f6e7839e2c5c33e342ef65c21430045f748
Arturo Collazo <accg14@gmail.com>
2018-09-08 12:34:13 -0300

    +Reference from User To Organization (includes migration and schema)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_07_235623) do
+ActiveRecord::Schema.define(version: 2018_09_08_151447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,9 @@ ActiveRecord::Schema.define(version: 2018_09_07_235623) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "organization_id"
+    t.index ["organization_id"], name: "index_users_on_organization_id"
   end
 
+  add_foreign_key "users", "organizations"
 end
