6d6889168af891b19c36b7af06c56817032775fa
Arturo Collazo <accg14@gmail.com>
2018-09-07 22:08:25 -0300

    Organization & User: +Model,+Controller,+Serializer,+Under active admin. Not done yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/create'
+  get 'users/destroy'
+  get 'users/show'
+  get 'users/update'
   mount SwaggerUiEngine::Engine, at: '/api_docs'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  resources :organizations do
+    resources :users
+  end
+  resources :users
 end
