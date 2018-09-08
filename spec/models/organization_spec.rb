e7b552b5b59abe298c1179dd5f05cccd11eab1a2
Arturo Collazo <accg14@gmail.com>
2018-09-08 15:25:47 -0300

    +Factory bots +rspec (some issues not resolved)

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    let(:organization) { FactoryBot.build(:organization) }
+
+    it 'should let create new ones, with appropiate data' do
+      expect(organization). to be_valid
+    end
+
+    it 'should not let create new ones, with nil name value' do
+      organization.name = nil
+      expect(organization).not_to be_valid
+    end
+
+    it 'should not let create new ones, with name value already taken' do
+
+    end
+  end
 end
