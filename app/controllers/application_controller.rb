fbf945da17eb08db6a578cf39305955e3c862608
Arturo Collazo <accg14@gmail.com>
2018-09-10 18:29:17 -0300

    list user full tested, good and bad test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound do
+    head 404
+  end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,13 @@
 FactoryBot.define do
   factory :user do
-    name "MyString"
-    surname "MyString"
-    ci "MyString"
-    email "MyString"
+    name 'nombre'
+    #name { Faker::Name.name }
+    surname 'apellido'
+    #surname { Faker::Name.name }
+    ci '123456789'
+    #ci { Faker::Number.number(8) }
+    email 'mail@correo.com'
+    #email { Faker::Faker::Internet.email }
     organization nil
   end
 end
