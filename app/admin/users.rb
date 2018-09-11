4b74a47dbb13f35f0359cb3a0c9ddb34715f67a4
frabago <fernando.rabago@fing.edu.uy>
2018-09-11 19:51:46 -0300

    Add error message creating and organization that already exist and fix rubocop warnings

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,15 +1,15 @@
 ActiveAdmin.register User do
-    belongs_to :organization, optional: true
-    permit_params :ci, :name, :surname, :email, :organization_id, organization_attributes: [:id, :name]
-
-    form do |f|
-        f.inputs "Details" do
-            f.input :ci, label: "User ci"
-            f.input :name, label: "User name"
-            f.input :surname, label: "User surname"
-            f.input :email, label: "User email"
-            f.input :organization_id, label: "Organization id" 
-        end
-        f.actions
+  belongs_to :organization, optional: true
+  permit_params :ci, :name, :surname, :email, :organization_id, organization_attributes: %(id name)
+  form do |f|
+    f.inputs 'Details' do
+      f.semantic_errors(*f.user.errors.keys)
+      f.input :ci, label: 'Ci'
+      f.input :name, label: 'Name'
+      f.input :surname, label: 'Surname'
+      f.input :email, label: 'Email'
+      f.input :organization_id, label: 'Organization id'
     end
+    f.actions
+  end
 end
