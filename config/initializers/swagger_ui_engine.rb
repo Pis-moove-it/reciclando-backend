5d6b65257d8bf756e48d2f9c9a5d98f6eb02edf1
Arturo Collazo <accg14@gmail.com>
2018-09-13 15:52:20 -0300

    documentation structure

--- config/initializers/swagger_ui_engine.rb
+++ config/initializers/swagger_ui_engine.rb
@@ -1,7 +1,7 @@
 SwaggerUiEngine.configure do |config|
   config.admin_username = ENV['ADMIN_USERNAME'] || 'reciclando'
   config.admin_password = ENV['ADMIN_PASSWORD'] || 'reciclando'
-  config.swagger_url = '/api/swagger.yaml'
+  config.swagger_url = '/docs/swagger.yaml'
   config.doc_expansion = 'list'
   config.request_headers = 'true'
 end
