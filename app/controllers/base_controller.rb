510457acbfc0038d8dc4ef1b21c83162963920b8
Federico Balarini <federico.balarini1995@gmail.com>
2018-09-12 22:27:38 -0300

    Minor fixes to base controller methods

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,13 +1,15 @@
 class BaseController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound do |exception|
-    render_error(3, exception)
-  end
-
   rescue_from StandardError do |exception|
     render_error(4, exception)
   end
 
-  def render_error(error_code, error_details)
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    render_error(3, exception)
+  end
+
+  private
+
+  def render_error(error_code, error_details = '')
     render json: Error.error_for(error_code, error_details),
            status: Error.status_for(error_code)
   end
