62a57824312a29fcba7a487b237709f23da8b146
Arturo Collazo <accg14@gmail.com>
2018-09-26 19:41:07 -0300

    fix in find_by

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,19 +4,17 @@ class AuthController < BaseController
     if check_credentials
       render json: { 'token' => device.auth_token }
     else
-      render_error(1, exception)
+      render_error(1, 'wrong credentials')
     end
   end
 
   private
 
   def device
-    #@device = Device.create_with(device_type: request.header['device_type']).find_or_create_by(request.header['device_id'])
     @device = Device.create_with(device_type: params[:device_type]).find_or_create_by(device_id: params[:device_id])
   end
 
   def check_credentials
-    #@check_credentials = Organization.find_by(name: params[:name]).try(:authenticate, params[:password])
-    @check_credentials = Organization.find_by(request.header['name']).try(:authenticate, request.header['password'])
+    @check_credentials = Organization.find_by(name: request.header['name']).try(:authenticate, request.header['password'])
   end
 end
