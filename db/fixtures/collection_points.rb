f8002ac86018f176904743b00826befb73cbed8a
Gastón Abellá <gaston.abellalopez@gmail.com>
2018-09-28 19:50:23 -0300

    Seeds added to Routes and CollectionPoints

---
+++ db/fixtures/collection_points.rb
@@ -0,0 +1,16 @@
+module Fixtures
+    COLLECTION_POINTS = [
+      {
+       latitude: '30.267153',
+       longitude: '-97.7430608'
+      },
+      {
+        latitude: '-13.843669',
+        longitude: '-60.894236'
+       },
+       {
+        latitude: '12.345678',
+        longitude: '98.765432'
+       }
+    ].freeze
+  end
