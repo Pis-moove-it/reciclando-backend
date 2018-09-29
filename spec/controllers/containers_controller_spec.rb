20c33b7e2f83062e703d18a81841a4571f98bf36
GonzaNunezCano <gonzalo.nunez.cano@gmail.com>
2018-09-29 16:46:17 -0300

    model and controller tests

--- spec/controllers/containers_controller_spec.rb
+++ spec/controllers/containers_controller_spec.rb
@@ -1,4 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe ContainersController, type: :controller do
+  let(:json_response) { JSON.parse(response.body, symbolize_keys: true) }
+  let!(:container) { FactoryBot.create(:container) }
+  let!(:updated_container) { FactoryBot.build(:container) }
+  let(:invalid_id) { Container.pluck(:id).max + 1 }
+
+  describe 'PUT #update' do
+    def update_container_call(id, status)
+      put :update, params: { id: id, container: { status: status } }
+    end
+
+    context 'when updating valid containers' do
+      it 'does return success' do
+        update_container_call(container.id, updated_container.status)
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'when updating non-existent containers' do
+      it 'does return not found' do
+        update_container_call(invalid_id, updated_container.status)
+        expect(json_response['error_code']).to eq 3
+      end
+    end
+
+    context 'when updating containers with wrong values' do
+      it 'does return bad request, nil status value' do
+        update_container_call(container.id, nil)
+        expect(json_response['error_code']).to eq 1
+      end
+      it 'does return internal error, invalid status value' do
+        update_container_call(container.id, 'invalid_value')
+        expect(json_response['error_code']).to eq 4
+      end
+    end
+  end
 end
