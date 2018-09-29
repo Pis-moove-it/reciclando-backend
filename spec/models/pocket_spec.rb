8e036075b451091958d78e8b5719e605e5ffa111
Federico Balarini <federico.balarini1995@gmail.com>
2018-09-29 19:26:31 -0300

    Add pocket serializer

--- spec/models/pocket_spec.rb
+++ spec/models/pocket_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Pocket, type: :model do
+  let!(:organization) { create(:organization) }
+  let!(:pocket) { FactoryBot.create(:pocket, organization: organization) }
+  let(:serializer) { PocketSerializer }
+
+  describe 'serializer' do
+    it 'does return pockets as specified in the serializer' do
+      expect(serializer.new(pocket).attributes.keys).to eq %i[id serial_number state]
+    end
+  end
 end
