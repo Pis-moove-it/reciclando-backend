5159f8c2524318c53e3c040bcb54c1a33ef30e49
GonzaNunezCano <gonzalo.nunez.cano@gmail.com>
2018-09-30 23:16:22 -0300

    the create form and index action were customized

--- app/admin/containers.rb
+++ app/admin/containers.rb
@@ -1,3 +1,21 @@
 ActiveAdmin.register Container do
   permit_params :status, :active
+
+  index do
+    id_column
+    column :status
+    column :active
+    actions
+  end
+
+  filter :status
+  filter :active
+
+  form do |f|
+    f.inputs do
+      f.input :status
+      f.input :active unless f.object.new_record?
+    end
+    f.actions
+  end
 end
