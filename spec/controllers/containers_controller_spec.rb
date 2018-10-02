6854e901fcb0e4c69d4f25b4b3c1e81b10c5bbfa
GonzaNunezCano <gonzalo.nunez.cano@gmail.com>
2018-10-02 00:54:49 -0300

    serializer test done

--- spec/controllers/containers_controller_spec.rb
+++ spec/controllers/containers_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ContainersController, type: :controller do
-  let(:json_response) { JSON.parse(response.body, symbolize_keys: true) }
+  let(:json_response) { JSON.parse(response.body, symbolize_names: true) }
   let!(:container) { FactoryBot.create(:container) }
   let!(:updated_container) { FactoryBot.build(:container) }
   let(:invalid_id) { Container.pluck(:id).max + 1 }
@@ -13,13 +13,13 @@ RSpec.describe ContainersController, type: :controller do
     end
 
     context 'when updating valid containers' do
+      let(:s_container) { Container.new(id: container.id, status: updated_container.status) }
       before(:each) { update_container_call(container.id, updated_container.status) }
-
       it 'does return success' do
         expect(response).to have_http_status(:ok)
       end
       it 'does return the container as specified in the serializer' do
-        expect(json_response.keys).to eq %w[id status active]
+        expect(json_response).to eq c_serializer.new(s_container).as_json
       end
     end
 
@@ -27,7 +27,7 @@ RSpec.describe ContainersController, type: :controller do
       it 'does return not found' do
         update_container_call(invalid_id, updated_container.status)
         expect(response).to have_http_status(404)
-        expect(json_response['error_code']).to eq 3
+        expect(json_response[:error_code]).to eq 3
       end
     end
 
@@ -35,12 +35,12 @@ RSpec.describe ContainersController, type: :controller do
       it 'does return bad request, nil status value' do
         update_container_call(container.id, nil)
         expect(response).to have_http_status(400)
-        expect(json_response['error_code']).to eq 1
+        expect(json_response[:error_code]).to eq 1
       end
       it 'does return internal error, invalid status value' do
         update_container_call(container.id, 'invalid_value')
         expect(response).to have_http_status(500)
-        expect(json_response['error_code']).to eq 4
+        expect(json_response[:error_code]).to eq 4
       end
     end
   end
