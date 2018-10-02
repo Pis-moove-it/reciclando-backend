dad36ed5ce1afb7c382ef779d3b48c5cd0998ea2
Federico Balarini <federico.balarini1995@gmail.com>
2018-10-01 21:19:55 -0300

    Add bales to admin system

--- app/admin/bales.rb
+++ app/admin/bales.rb
@@ -1,3 +1,20 @@
 ActiveAdmin.register Bale do
   permit_params :weight, :material
+
+  filter :weight
+  filter :material, as: :select, collection: Bale.materials
+  filter :user
+
+  index do
+    id_column
+    column :weight
+    column :material
+    column :user
+  end
+
+  show title: :id do
+    attributes_table do
+      rows :id, :weight, :material, :user
+    end
+  end
 end
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,6 +1,8 @@
 ActiveAdmin.register User do
   belongs_to :organization, optional: true
+
   permit_params :ci, :name, :surname, :email, :organization_id
+
   form do |f|
     f.inputs 'Details' do
       f.semantic_errors(*f.user.errors.keys)
@@ -11,5 +13,34 @@ ActiveAdmin.register User do
       f.input :organization_id, label: 'Organization id'
     end
     f.actions
+  end
+
+  filter :name
+  filter :surname
+  filter :ci
+  filter :email
+  filter :organization
+  filter :created_at
+
+  index do
+    id_column
+    column :name
+    column :surname
+    column :organization
+    actions
+  end
+
+  show title: :name do
+    attributes_table do
+      rows :id, :name, :surname, :ci, :email, :organization
+    end
+
+    panel 'Bales' do
+      table_for(user.bales) do |_bale|
+        column(:id) { |bale| link_to(bale.id, admin_bale_path(bale)) }
+        column(:material, &:material)
+        column(:weight, &:weight)
+      end
+    end
   end
 end
