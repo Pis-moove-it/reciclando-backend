45a0a3c871aa386af034e95fbc66d2c4a5b00710
Arturo Collazo <accg14@gmail.com>
2018-10-01 23:06:29 -0300

    several fixes in controllers

--- app/controllers/authenticate_controller.rb
+++ app/controllers/authenticate_controller.rb
@@ -1,14 +1,35 @@
 class AuthenticateController < BaseController
   before_action :authenticated_user
 
+  # authenticate OR create a device
+  def authenticate_device_with(organization)
+    device = device_for(organization)
+    response.set_header('ApiKey', device.auth_token) if device
+  end
+
   private
 
   # This method verify if the user doing the request is authenticated.
   # If not, renders an Unauthorized error.
-  #
+
   def authenticated_user
     return render_error(2, 'Invalid auth token') if logged_device.nil?
     logged_device
+  end
+
+  def device_for(organization)
+    Device.transaction do
+      device = Device.create_with(device_type: device_type).find_or_create_by!(device_id: device_id)
+      device.update!(organization: organization)
+    end
+  end
+
+  def device_type
+    request.headers['HTTP_DEVICE_TYPE']
+  end
+
+  def device_id
+    request.headers['HTTP_DEVICE_ID']
   end
 
   def logged_device
--- app/controllers/organization_controller.rb
+++ app/controllers/organization_controller.rb
@@ -1,10 +1,19 @@
-class OrganizationController < ApplicationController
-  def testname
-    authenticate(organization_by_name)
+class OrganizationController < AuthenticateController
+  skip_before_action :authenticated_user
+
+  def login
+    return render_error(1, 'wrong credentials') unless check_credentials_of(organization_by_name)
+    authenticate_device_with(organization)
+    render json: organization_by_name
   end
-  # private
+
+  private
 
   def organization_by_name
     @organization_by_name ||= Organization.find_by!(name: params['name'])
+  end
+
+  def check_credentials_of(organization)
+    organization.authenticate(params['password'])
   end
 end
