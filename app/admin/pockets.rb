4f903fa1590059ff3eed2b65db936cbe502da279
GonzaNunezCano <gonzalo.nunez.cano@gmail.com>
2018-10-03 19:13:37 -0300

    edit serial number in activeAdmin

--- app/admin/pockets.rb
+++ app/admin/pockets.rb
@@ -1,11 +1,28 @@
 ActiveAdmin.register Pocket do
-  actions :index
+  permit_params :serial_number
 
   index do
     id_column
     column :serial_number
     column :state
     column :organization
+    actions
+  end
+
+  filter :serial_number
+  filter :state
+  filter :organization
+
+  form do |f|
+    f.inputs do
+      f.input :serial_number if f.object.persisted?
+      if f.object.new_record?
+        f.input :serial_number
+        f.input :state
+        f.input :organization
+      end
+    end
+    f.actions
   end
 
   show title: :id do
