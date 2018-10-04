db1f9d2d91797450f6206323a618ee3530b898bf
GonzaNunezCano <gonzalo.nunez.cano@gmail.com>
2018-10-04 20:34:38 -0300

    update pocket weight in activeAdmin + pockets in fixtures folder was updated

--- app/admin/pockets.rb
+++ app/admin/pockets.rb
@@ -1,35 +1,34 @@
 ActiveAdmin.register Pocket do
-  permit_params :serial_number
+  permit_params :serial_number, :weight
   actions :index, :show, :edit, :update
 
   index do
     id_column
     column :serial_number
+    column :weight
     column :state
     column :organization
     actions
   end
 
   filter :serial_number
+  filter :weight
   filter :state
   filter :organization
 
   show title: :serial_number do
     attributes_table do
-      rows :id, :serial_number, :state, :organization
+      rows :id, :serial_number, :weight, :state, :organization
     end
   end
 
   form do |f|
     f.inputs do
-      f.input :serial_number if f.object.persisted?
+      if f.object.persisted?
+        f.input :serial_number
+        f.input :weight
+      end
     end
     f.actions
-  end
-
-  show title: :id do
-    attributes_table do
-      rows :id, :serial_number, :state, :organization
-    end
   end
 end
--- db/fixtures/pockets.rb
+++ db/fixtures/pockets.rb
@@ -2,15 +2,21 @@ module Fixtures
   POCKETS = [
     {
       serial_number: '123451',
-      organization_id: 1
+      weight: 10.5,
+      organization_id: 1,
+      collection_id: 1
     },
     {
       serial_number: '123452',
-      organization_id: 1
+      weight: 2.3,
+      organization_id: 1,
+      collection_id: 1
     },
     {
       serial_number: '123453',
-      organization_id: 1
-    },
+      weight: 14.8,
+      organization_id: 1,
+      collection_id: 1
+    }
   ].freeze
 end
