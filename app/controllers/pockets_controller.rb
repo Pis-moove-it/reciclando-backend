8da61a8760046932a3b64e267389e1f530078df9
Arturo Collazo <accg14@gmail.com>
2018-10-06 22:24:14 -0300

    swagger corrected, check_entry idem

--- app/controllers/pockets_controller.rb
+++ app/controllers/pockets_controller.rb
@@ -5,7 +5,7 @@ class PocketsController < AuthenticateController
 
   def edit_serial_number
     pocket = Pocket.find(params[:id])
-    return render_error(1, 'Missing serial number') if check_entry
+    return render_error(1, 'Missing serial number') if check_invalid_entry
 
     pocket.update!(serial_number: params['serial_number'])
     render json: pocket
@@ -13,7 +13,7 @@ class PocketsController < AuthenticateController
 
   private
 
-  def check_entry
+  def check_invalid_entry
     params['serial_number'].nil? || params['serial_number'].blank?
   end
 end
