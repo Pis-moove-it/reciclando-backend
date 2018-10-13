1010f6c1155363b13a56005519932ff6a9851486
Federico Balarini <federico.balarini1995@gmail.com>
2018-10-13 16:42:07 -0300

    Adds fake questions

---
+++ db/fixtures/questions.rb
@@ -0,0 +1,84 @@
+module Fixtures
+  QUESTIONS = [
+    {
+      question: '¿Cuánto genera en promedio de basura una persona al día?',
+      option_a: '1 kg',
+      option_b: '125 kg',
+      option_c: '2 kg',
+      option_d: '750 kg',
+      correct_option: 'A'
+    },
+    {
+      question: '¿Qué porcentaje de los residuos son reciclados en Uruguay?',
+      option_a: '3%',
+      option_b: '15%',
+      option_c: '47%',
+      option_d: '72%',
+      correct_option: 'B'
+    },
+    {
+      question: '¿Cuál de estos materiales puede ser reciclado infinitas veces?',
+      option_a: 'Cartón',
+      option_b: 'Plástico',
+      option_c: 'Papel',
+      option_d: 'Vidrio',
+      correct_option: 'D'
+    },
+    {
+      question: '¿Cuál de estos materiales puede ser reciclado infinitas veces?',
+      option_a: 'Plástico',
+      option_b: 'Papel',
+      option_c: 'Metal',
+      option_d: 'Cartón',
+      correct_option: 'C'
+    },
+    {
+      question: '¿Cuál es el tiempo de uso promedio de una bolsa de plástico?',
+      option_a: '12 a 20 minutos',
+      option_b: '40 minutos',
+      option_c: '2 minutos',
+      option_d: '3 horas',
+      correct_option: 'A'
+    },
+    {
+      question: '¿Cuántos árboles podrían ser ahorrados si se reciclaran todos los periódicos en un año?',
+      option_a: '500 mil',
+      option_b: 'Más de 1 millón',
+      option_c: '1 millón',
+      option_d: 'Menos de 100 mil',
+      correct_option: 'B'
+    },
+    {
+      question: '¿Cuántos árboles se salvan al reciclar 1 tonelada de papel?',
+      option_a: '50',
+      option_b: '2',
+      option_c: '5',
+      option_d: '17',
+      correct_option: 'D'
+    },
+    {
+      question: '¿Qué porcentaje de las bolsas de plástico son recicladas a nivel mundial?',
+      option_a: '70%',
+      option_b: '8%',
+      option_c: '1%',
+      option_d: '35%',
+      correct_option: 'C'
+    },
+    {
+      question: '¿Cuántos litros de agua podría contaminar el mercurio de una pila de botón?',
+      option_a: '6',
+      option_b: '60',
+      option_c: '60 mil',
+      option_d: '600 mil',
+      correct_option: 'D'
+    },
+    {
+      question: '¿Cuántos litros de agua puede contaminar 1 litro de aceite?',
+      option_a: '1000',
+      option_b: '3000',
+      option_c: '10',
+      option_d: '100',
+      correct_option: 'A'
+    }
+  ].freeze
+end
