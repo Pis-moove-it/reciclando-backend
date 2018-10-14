268ee4c616195d044dabe74d6cc09f49aa22180c
Arturo Collazo <accg14@gmail.com>
2018-10-13 23:21:41 -0300

    serializer with pockets

--- app/serializers/route_serializer.rb
+++ app/serializers/route_serializer.rb
@@ -1,5 +1,13 @@
 class RouteSerializer < ActiveModel::Serializer
-  attributes :id, :length, :travel_image, :created_at
+  attributes :id, :length, :travel_image, :created_at, :pockets, :number_of_pockets
 
   belongs_to :user
+
+  attribute :pockets do
+    object.collections.map(&:pockets).flatten
+  end
+
+  attribute :number_of_pockets do
+    object.collections.map(&:pockets).flatten.count
+  end
 end
