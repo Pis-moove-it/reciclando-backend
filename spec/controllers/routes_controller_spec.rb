1a46dba29636fb8959119d444b97990b3cf8e7ef
Gastón Abellá <gaston.abellalopez@gmail.com>
2018-10-16 17:01:44 -0300

    Controller tests finished

--- spec/controllers/routes_controller_spec.rb
+++ spec/controllers/routes_controller_spec.rb
@@ -43,13 +43,11 @@ RSpec.describe RoutesController, type: :controller do
   end
 
   describe 'PUT #update' do
-    let(:ended_route) { attributes_for(:ended_route) }
+    let(:ended_route) { build(:ended_route) }
 
     def end_route_call(route_id, length, travel_image)
       put :update, params: { id: route_id, route: { length: length, travel_image: travel_image } }, as: :json
     end
-
-    before(:each) { end_route_call(route.id, ended_route[:length], ended_route[:travel_image]) }
 
     context 'when user is authenticated' do
       let!(:auth_user) { create_an_authenticated_user_with(organization, '1', 'android') }
@@ -57,18 +55,79 @@ RSpec.describe RoutesController, type: :controller do
 
       let(:r_serializer) { RouteSerializer }
 
-      it 'does return success' do
-        expect(response).to have_http_status(:ok)
+      context 'when inputs are valid' do
+        before(:each) { end_route_call(route.id, ended_route[:length], ended_route[:travel_image]) }
+
+        it 'does return success' do
+          expect(response).to have_http_status(:ok)
+        end
+
+        it 'does update the route' do
+          route.reload
+          expect([route.length, route.travel_image]).to eql [ended_route[:length], ended_route[:travel_image]]
+        end
+
+        it 'does return the route as specified in the serializer' do
+          expect(json_response).to eql r_serializer.new(route.reload).as_json
+        end
       end
 
-      it 'does return the route as specified in the serializer' do
-        expect(json_response).to eql r_serializer.new(Route.first).as_json
+      context 'when length is missing' do
+        before(:each) { end_route_call(route.id, nil, ended_route[:travel_image]) }
+
+        it 'does return the right error' do
+          expect(response).to have_http_status(400)
+        end
+
+        it 'does return missing length' do
+          expect(json_response[:details]).to eql 'Missing length'
+        end
+      end
+
+      context 'when length is negative' do
+        before(:each) { end_route_call(route.id, -13, ended_route[:travel_image]) }
+
+        it 'does return the right error' do
+          expect(response).to have_http_status(400)
+        end
+
+        it 'does return negative length' do
+          expect(json_response[:details]).to eql 'Negative length'
+        end
+      end
+
+      context 'when travel image is missing' do
+        before(:each) { end_route_call(route.id, ended_route[:length], nil) }
+
+        it 'does return the right error' do
+          expect(response).to have_http_status(400)
+        end
+
+        it 'does return missing travel image' do
+          expect(json_response[:details]).to eql 'Missing travel image'
+        end
+      end
+
+      context 'when route is ended' do
+        let(:another_ended_route) { create(:ended_route, user: auth_user) }
+
+        before(:each) { end_route_call(another_ended_route.id, ended_route[:length], ended_route[:travel_image]) }
+
+        it 'does return the right error' do
+          expect(response).to have_http_status(400)
+        end
+
+        it 'does return route ended' do
+          expect(json_response[:details]).to eql 'Route already ended'
+        end
       end
     end
 
     context 'when user is not authenticated' do
       let!(:user) { create(:user, organization: organization) }
       let!(:route) { create(:route, user: user) }
+
+      before(:each) { end_route_call(route.id, ended_route[:length], ended_route[:travel_image]) }
 
       it 'does return an error' do
         expect(response).to have_http_status(401)
--- spec/factories/routes.rb
+++ spec/factories/routes.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
   end
 
   factory :ended_route, parent: :route do
-    length 13.5 #{ Faker::Number.number(2) }
+    length { Faker::Number.number(2) }
     travel_image { Faker::Internet.url }
   end
 end
