3704777bc549d79d898ab087db1ac7588b09000c
Gastón Abellá <gaston.abellalopez@gmail.com>
2018-10-18 14:03:58 -0300

    Details fixed in route controller

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -26,13 +26,9 @@ class RoutesController < AuthenticateController
 
   def validate_ended_route
     return render_error(1, 'Missing length') if route_params['length'].blank?
-    return render_error(1, 'Negative length') if negative_length?
+    return render_error(1, 'Negative length') if route_params['length'].negative?
     return render_error(1, 'Missing travel image') if route_params['travel_image'].blank?
     return render_error(1, 'Route already ended') if route.ended?
-  end
-
-  def negative_length?
-    route_params[:length].negative?
   end
 
   def route_params
