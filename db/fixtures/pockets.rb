40557e32c14710b669846b3449f04337dee582bc
GonzaNunezCano <gonzalo.nunez.cano@gmail.com>
2018-10-25 21:31:08 -0300

    db/fixtures/pockets.rb corrected

--- db/fixtures/pockets.rb
+++ db/fixtures/pockets.rb
@@ -2,18 +2,19 @@ module Fixtures
   POCKETS = [
     {
       serial_number: '123451',
+      weight: nil,
       organization_id: 1,
       collection_id: 1
     },
     {
       serial_number: '123452',
-      weight: 0,
+      weight: nil,
       organization_id: 1,
       collection_id: 1
     },
     {
       serial_number: '123453',
-      weight: 0,
+      weight: nil,
       organization_id: 1,
       collection_id: 1
     }
