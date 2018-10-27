adae8e69f9e8bd33fb7ae82822c62a1f4a45c72c
Gastón Abellá <gaston.abellalopez@gmail.com>
2018-10-27 14:33:06 -0300

    Revert "Status_id returned in the container serializer"
    This reverts commit 9bba86c0e696d7d9a21cd5ef85ef59f76e959252.

--- app/serializers/container_serializer.rb
+++ app/serializers/container_serializer.rb
@@ -1,7 +1,3 @@
 class ContainerSerializer < ActiveModel::Serializer
-  attributes :id, :latitude, :longitude, :status_id
-
-  def status_id
-    read_attribute_for_serialization(:status)
-  end
+  attributes :id, :latitude, :longitude, :status
 end
