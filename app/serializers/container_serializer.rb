b4eb3a1b27f163bd466e9fe3a487af86de5ef0c2
Gastón Abellá <gaston.abellalopez@gmail.com>
2018-10-27 12:08:19 -0300

    Status_id returned in the container serializer

--- app/serializers/container_serializer.rb
+++ app/serializers/container_serializer.rb
@@ -1,3 +1,7 @@
 class ContainerSerializer < ActiveModel::Serializer
-  attributes :id, :latitude, :longitude, :status
+  attributes :id, :latitude, :longitude, :status_id
+
+  def status_id
+    read_attribute_for_serialization(:status)
+  end
 end
