46dd476f14d8fca4cb69a99edfffba408debbb2d
Gastón Abellá <gaston.abellalopez@gmail.com>
2018-11-01 20:33:03 -0300

    Test added for #classify in pockets

--- spec/models/pocket_spec.rb
+++ spec/models/pocket_spec.rb
@@ -57,4 +57,41 @@ RSpec.describe Pocket, type: :model do
       expect(pocket).not_to be_valid
     end
   end
+
+  describe 'functions' do
+    let(:pocket) { build(:weighed_pocket, organization: organization, collection: collection) }
+
+    describe '#classify' do
+      describe 'with valid parameters' do
+        let!(:kg_trash) { Faker::Number.decimal(2, 2).to_f }
+        let!(:kg_plastic) { Faker::Number.decimal(2, 2).to_f }
+        let!(:kg_glass) { Faker::Number.decimal(2, 2).to_f }
+
+        let!(:total_weight) { kg_trash + kg_plastic + kg_glass }
+        let!(:percentage) { pocket.weight / total_weight.to_f }
+
+        before(:each) { pocket.classify(total_weight, kg_trash, kg_plastic, kg_glass) }
+
+        it 'does be valid' do
+          expect(pocket).to be_valid
+        end
+
+        it 'does has correct kg_trash' do
+          expect(pocket.kg_trash).to eql(percentage * kg_trash)
+        end
+
+        it 'does has correct kg_recycled_plastic' do
+          expect(pocket.kg_recycled_plastic).to eql(percentage * kg_plastic)
+        end
+
+        it 'does has correct kg_recycled_glass' do
+          expect(pocket.kg_recycled_glass).to eql(percentage * kg_glass)
+        end
+
+        it 'does be classified' do
+          expect(pocket.state).to eql 'Classified'
+        end
+      end
+    end
+  end
 end
