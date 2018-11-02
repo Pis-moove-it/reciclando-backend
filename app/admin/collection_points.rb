1b0be4e9bf8b1b671c4e596cda1f4e5c2ae558bf
Arturo Collazo <accg14@gmail.com>
2018-11-02 17:52:02 -0300

    no created at in index

--- app/admin/collection_points.rb
+++ app/admin/collection_points.rb
@@ -1,6 +1,20 @@
 ActiveAdmin.register CollectionPoint do
   permit_params :latitude, :longitude, :status, :active, :kg_recycled_glass, :kg_trash, :kg_recycled_plastic
 
+
+
+
+  index do
+    id_column
+    column :latitude
+    column :longitude
+    column :status
+    column :active
+    column :kg_recycled_glass
+    column :kg_recycled_plastic
+    column :kg_trash
+  end
+
   filter :latitude
   filter :longitude
   filter :kg_trash
