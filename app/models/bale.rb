229a646db72049af3f19f42e1f627e9c59ea1b0a
GonzaNunezCano <gonzalo.nunez.cano@gmail.com>
2018-11-03 12:15:57 -0300

    create bale in active admin corrected

--- app/models/bale.rb
+++ app/models/bale.rb
@@ -1,6 +1,8 @@
 class Bale < ApplicationRecord
   enum material: %i[Trash Plastic Glass]
-  validates :weight, :material, presence: true
+
+  validates :material, presence: true
+  validates :weight, presence: true, numericality: { greater_than_or_equal_to: 0.001 }
 
   belongs_to :organization
   belongs_to :user
