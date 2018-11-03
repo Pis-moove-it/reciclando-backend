2326322434f45a0c9b9a78632259357e79343b70
Gastón Abellá <gaston.abellalopez@gmail.com>
2018-11-03 18:58:56 -0300

    Some corrections in ClassificationController

--- app/controllers/classification_controller.rb
+++ app/controllers/classification_controller.rb
@@ -1,9 +1,5 @@
 class ClassificationController < AuthenticateController
   def create
-    pockets = Pocket.where(id: classification_params[:pocket_ids], organization: logged_user.organization)
-    return render_error(1, 'Negative kg of trash') if classification_params[:kg_trash].negative?
-    return render_error(1, 'Negative kg of plastic') if classification_params[:kg_plastic].negative?
-    return render_error(1, 'Negative kg of glass') if classification_params[:kg_glass].negative?
     return render_error(1, 'Unweighed or classified pockets') unless pockets.all?(&:Weighed?)
     pockets.each do |p|
       p.classify(pockets.sum(&:weight), classification_params[:kg_trash],
@@ -14,6 +10,10 @@ class ClassificationController < AuthenticateController
   end
 
   private
+
+  def pockets
+    @pockets ||= Pocket.where(id: classification_params[:pocket_ids], organization: logged_user.organization)
+  end
 
   def classification_params
     params.permit(:kg_trash, :kg_plastic, :kg_glass, pocket_ids: [])
