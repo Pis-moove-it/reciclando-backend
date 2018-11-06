77c19210f931ac1fd15a290e664cd158bab7318a
Federico Balarini <federico.balarini1995@gmail.com>
2018-11-06 18:17:15 -0300

    Adds more collections to seed

--- db/fixtures/collections.rb
+++ db/fixtures/collections.rb
@@ -3,6 +3,86 @@ module Fixtures
     {
       collection_point_id: 1,
       route_id: 1
+    },
+    {
+      collection_point_id: 2,
+      route_id: 2
+    },
+    {
+      collection_point_id: 3,
+      route_id: 3
+    },
+    {
+      collection_point_id: 4,
+      route_id: 1
+    },
+    {
+      collection_point_id: 5,
+      route_id: 2
+    },
+    {
+      collection_point_id: 1,
+      route_id: 3
+    },
+    {
+      collection_point_id: 2,
+      route_id: 1
+    },
+    {
+      collection_point_id: 3,
+      route_id: 2
+    },
+    {
+      collection_point_id: 4,
+      route_id: 3
+    },
+    {
+      collection_point_id: 5,
+      route_id: 1
+    },
+    {
+      collection_point_id: 1,
+      route_id: 2
+    },
+    {
+      collection_point_id: 2,
+      route_id: 3
+    },
+    {
+      collection_point_id: 3,
+      route_id: 1
+    },
+    {
+      collection_point_id: 4,
+      route_id: 2
+    },
+    {
+      collection_point_id: 5,
+      route_id: 3
+    },
+    {
+      collection_point_id: 1,
+      route_id: 1
+    },
+    {
+      collection_point_id: 2,
+      route_id: 2
+    },
+    {
+      collection_point_id: 3,
+      route_id: 3
+    },
+    {
+      collection_point_id: 4,
+      route_id: 1
+    },
+    {
+      collection_point_id: 5,
+      route_id: 2
+    },
+    {
+      collection_point_id: 1,
+      route_id: 3
     }
   ].freeze
 end
