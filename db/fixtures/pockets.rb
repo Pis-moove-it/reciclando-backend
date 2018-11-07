0b9d92c90ad44507301bca31e9abc55ba6b2fb32
Arturo Collazo <accg14@gmail.com>
2018-11-06 21:08:12 -0300

    more pockets to test order in index

--- db/fixtures/pockets.rb
+++ db/fixtures/pockets.rb
@@ -17,6 +17,27 @@ module Fixtures
       weight: nil,
       organization_id: 1,
       collection_id: 1
+    },
+    {
+      serial_number: '123454',
+      weight: nil,
+      organization_id: 1,
+      collection_id: 1,
+      check_in: Time.current
+    },
+    {
+      serial_number: '123455',
+      weight: nil,
+      organization_id: 1,
+      collection_id: 1,
+      check_in: Time.current + 10
+    },
+    {
+      serial_number: '123456',
+      weight: nil,
+      organization_id: 1,
+      collection_id: 1,
+      check_in: Time.current + 20
     }
   ].freeze
 end
