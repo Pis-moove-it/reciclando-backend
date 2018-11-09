8447c2a1ef48ab12bbe3265ffb16746c92c1b64d
Arturo Collazo <accg14@gmail.com>
2018-11-09 17:21:16 -0300

    container serializer + description

--- db/fixtures/containers.rb
+++ db/fixtures/containers.rb
@@ -5,35 +5,40 @@ module Fixtures
       longitude: '-56.164705',
       status: 'Ok',
       active: 'True',
-      organization_id: 1
+      organization_id: 1,
+      description: 'Escuela n° 51'
     },
     {
       latitude: '-34.914914',
       longitude: '-56.165209',
       status: 'Ok',
       active: 'True',
-      organization_id: 1
+      organization_id: 1,
+      description: 'Rocha Fútbol Club'
     },
     {
       latitude: '-34.919691',
       longitude: '-56.166363',
       status: 'Ok',
       active: 'True',
-      organization_id: 1
+      organization_id: 1,
+      description: 'Intendencia'
     },
     {
       latitude: '-34.916611',
       longitude: '-56.162675',
       status: 'Damaged',
       active: 'True',
-      organization_id: 1
+      organization_id: 1,
+      description: 'Udelar Rocha'
     },
     {
       latitude: '-34.919440',
       longitude: '-56.162403',
       status: 'Removed',
       active: 'False',
-      organization_id: 2
+      organization_id: 2,
+      description: 'Hospital Rocha'
     }
   ].freeze
 end
