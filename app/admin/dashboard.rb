e4d9ea016ced502b64a939e35b70bae627d06643
Federico Balarini <federico.balarini1995@gmail.com>
2018-11-11 20:09:11 -0300

    Adds organization select and events checkbox

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,19 +2,30 @@ ActiveAdmin.register_page 'Dashboard' do
   menu priority: 1, label: proc { I18n.t('active_admin.dashboard') }
 
   page_action :report, method: :post do
-    redirect_to admin_dashboard_path(organization_id: params['organization_id'])
+    redirect_to admin_dashboard_path(organization_id: params['organization_id'], all: params['all'])
   end
 
   content title: proc { I18n.t('active_admin.dashboard') } do
+    # Use organization_id param to filter pockets and bales
+    organization_id = params[:organization_id] || 1
+
     columns do
       column do
         panel 'Reporte correspondiente a', style: 'max-width: 350px; margin: auto; margin-bottom: 20px;' do
-          form action: 'dashboard/report', method: :post do |f|
+          form action: admin_dashboard_report_path, method: :post do |f|
             f.input :authenticity_token, type: :hidden, name: :authenticity_token, value: form_authenticity_token
             table style: 'max-width: 300px;' do
               tr do
                 td { f.label 'Organización: ' }
-                td { f.input :organization_id, type: :string, name: 'organization_id' }
+                td do
+                  select name: 'organization_id' do
+                    Organization.all.each do |organization|
+                      option value: organization.id, selected: organization_id.eql?(organization.id.to_s) do
+                        organization.name
+                      end
+                    end
+                  end
+                end
               end
               tr do
                 td { f.label 'Día: ' }
@@ -22,7 +33,7 @@ ActiveAdmin.register_page 'Dashboard' do
               end
               tr do
                 td { f.label 'Todos los eventos del día: ' }
-                td { f.input :all, type: :checkbox, name: 'all' }
+                td { f.input :all, type: :checkbox, name: 'all', checked: params[:all] }
               end
               tr do
                 td {}
@@ -31,7 +42,7 @@ ActiveAdmin.register_page 'Dashboard' do
             end
           end
         end
-        organization = params[:organization_id] || 1
+
         panel 'Bolsones' do
           table do
             thead do
@@ -44,54 +55,59 @@ ActiveAdmin.register_page 'Dashboard' do
             tbody do
               tr style: 'font-size: 150%' do
                 td 'Totales: '
-                td Pocket.where(organization_id: organization).count
-                td Pocket.where(organization_id: organization).pluck(:weight).map { |weight| weight || 0 }.sum.round
+                td Pocket.where(organization_id: organization_id).count
+                td Pocket.where(organization_id: organization_id).pluck(:weight).map { |weight| weight || 0 }.sum.round
               end
               tr style: 'font-size: 150%' do
                 td 'No Pesados: '
-                td Pocket.where(organization_id: organization).unweighed.count
+                td Pocket.where(organization_id: organization_id).unweighed.count
                 td '-'
               end
               tr style: 'font-size: 150%' do
                 td 'Pesados: '
-                td Pocket.where(organization_id: organization).weighed.count
-                td Pocket.where(organization_id: organization).weighed.pluck(:weight).sum.round
+                td Pocket.where(organization_id: organization_id).weighed.count
+                td Pocket.where(organization_id: organization_id).weighed.pluck(:weight).sum.round
               end
             end
           end
         end
-        panel 'Fardos' do
-          table do
-            thead do
-              tr do
-                th ''
-                th :Cantidad
-                th :Peso
+
+        # Show this panel only if checkbox is checked
+        if params[:all]
+          panel 'Fardos' do
+            table do
+              thead do
+                tr do
+                  th ''
+                  th :Cantidad
+                  th :Peso
+                end
               end
-            end
-            tbody do
-              tr style: 'font-size: 150%' do
-                td 'Basura: '
-                td Bale.where(organization_id: organization).trash.count
-                td Bale.where(organization_id: organization).trash.pluck(:weight).sum.round
-              end
-              tr style: 'font-size: 150%' do
-                td 'Plástico: '
-                td Bale.where(organization_id: organization).plastic.count
-                td Bale.where(organization_id: organization).plastic.pluck(:weight).sum.round
-              end
-              tr style: 'font-size: 150%' do
-                td 'Vidrio: '
-                td Bale.where(organization_id: organization).glass.count
-                td Bale.where(organization_id: organization).glass.pluck(:weight).sum.round
+              tbody do
+                tr style: 'font-size: 150%' do
+                  td 'Basura: '
+                  td Bale.where(organization_id: organization_id).trash.count
+                  td Bale.where(organization_id: organization_id).trash.pluck(:weight).sum.round
+                end
+                tr style: 'font-size: 150%' do
+                  td 'Plástico: '
+                  td Bale.where(organization_id: organization_id).plastic.count
+                  td Bale.where(organization_id: organization_id).plastic.pluck(:weight).sum.round
+                end
+                tr style: 'font-size: 150%' do
+                  td 'Vidrio: '
+                  td Bale.where(organization_id: organization_id).glass.count
+                  td Bale.where(organization_id: organization_id).glass.pluck(:weight).sum.round
+                end
               end
             end
           end
-        end
-        h2 style: 'text-align: center;' do
-          "Diferencia entre enfardado y pesado (Enfardado - Pesado):
-            #{Bale.where(organization_id: organization).pluck(:weight).sum.round -
-              Pocket.where(organization_id: organization).pluck(:weight).map { |weight| weight || 0 }.sum.round} kg"
+          h2 style: 'text-align: center;' do
+            "Diferencia entre enfardado y pesado (Enfardado - Pesado):
+              #{Bale.where(organization_id: organization_id).pluck(:weight).sum.round -
+                Pocket.where(organization_id: organization_id).pluck(:weight)
+                .map { |weight| weight || 0 }.sum.round} kg"
+          end
         end
       end
     end
